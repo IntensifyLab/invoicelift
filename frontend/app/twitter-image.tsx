eb316981be01aec0890dc2c244508303b5bce282
james-t <james.t.dev@gmail.com>
2025-10-28 05:39:18 +0100

    perf: optimize memory footprint and execution paths (commit #108)

--- frontend/app/twitter-image.tsx
+++ frontend/app/twitter-image.tsx
@@ -93,3 +93,5 @@ export default function Image() {
 // Contribution check by sambuilder at 2025-04-18T18:37:14
 
 // Contribution check by robert-j at 2025-07-24T00:08:16
+
+// Contribution check by james-t at 2025-10-28T05:39:18
