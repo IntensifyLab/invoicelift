e8658052f79f87d881321670a3b1def9c76c4003
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-12-11 21:55:51 +0100

    feat: implement validation checks for core modules (commit #121)

--- contract/invoice-registry/src/lib.rs
+++ contract/invoice-registry/src/lib.rs
@@ -34,3 +34,5 @@ impl InvoiceRegistry {
 // Contribution check by johndoedev at 2025-06-02T10:53:47
 
 // Contribution check by nancy-k at 2025-09-06T16:24:49
+
+// Contribution check by oluwagbemiga at 2025-12-11T21:55:51
